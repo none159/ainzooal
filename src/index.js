7a5f76ab89add1a28aaa281a236e13709172ca19
none159 <noneenone019@gmail.com>
2022-08-29 12:56:22 +0100

    commited

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-      <App />
+     <BrowserRouter>
+      
+      </BrowserRouter>
     
   </React.StrictMode>
 );
