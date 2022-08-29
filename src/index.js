ee0082610c18be9251071dcef972aa6386fe4ab6
none159 <noneenone019@gmail.com>
2022-08-29 12:47:34 +0100

    commited

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basemname={`/${process.env.PUBLIC_URL}`}>
+    
       <App />
-    </BrowserRouter>
+    
   </React.StrictMode>
 );
 
